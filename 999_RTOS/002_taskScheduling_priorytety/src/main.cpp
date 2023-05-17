9c1d2e63459ccc41c8fcc7fa7b80095599a565f9
DamianPodrazaCode <damian.podraza@gmail.com>
2023-05-17 18:16:03 +0200

    modified:   999_RTOS/002_taskScheduling_priorytety/src/main.cpp

--- 999_RTOS/002_taskScheduling_priorytety/src/main.cpp
+++ 999_RTOS/002_taskScheduling_priorytety/src/main.cpp
@@ -6,7 +6,7 @@ int outPin2 = 19;
 //int outPin3 = 2;
 
 
-TaskHandle_t* hTaskPin1;
+TaskHandle_t hTaskPin1 = NULL;
 void taskPin1(void* par) {
   while (1) {
     digitalWrite(outPin1, 1);
@@ -16,7 +16,7 @@ void taskPin1(void* par) {
   }
 }
 
-TaskHandle_t* hTaskPin2;
+TaskHandle_t hTaskPin2 = NULL;
 void taskPin2(void* par) {
   while (1) {
     for (int i = 0; i < 100000; i++) { // ta pętla trwa kilkadziesiąt milisekund i przerwie taska o mniejszym priorytecie
@@ -33,20 +33,22 @@ void setup() {
   pinMode(outPin2, OUTPUT);
   //pinMode(outPin3, OUTPUT);
 
-  xTaskCreatePinnedToCore(taskPin1, "out1", 1024, NULL, 1, hTaskPin1, app_cpu);
-  xTaskCreatePinnedToCore(taskPin2, "out2", 1024, NULL, 2, hTaskPin2, app_cpu);
+  xTaskCreatePinnedToCore(taskPin1, "out1", 1024, NULL, 1, &hTaskPin1, app_cpu);
+  xTaskCreatePinnedToCore(taskPin2, "out2", 1024, NULL, 2, &hTaskPin2, app_cpu);
 
 }
 
 void loop() {
   if (millis() > 5000) {
     if (hTaskPin2 != NULL) {
-
+      vTaskDelete(hTaskPin2);
+      hTaskPin2 = NULL;
     }
   }
   if (millis() > 7000) {
     if (hTaskPin1 != NULL) {
-
+      vTaskDelete(hTaskPin1);
+      hTaskPin1 = NULL;
     }
   }
 }
