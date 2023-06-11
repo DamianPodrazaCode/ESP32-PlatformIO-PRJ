b1862acb11b02a693bfead1a4afdb8c7755fb147
DamianPodrazaCode <damian.podraza@gmail.com>
2023-06-11 10:03:49 +0200

    new file:   003_softTimer/include/SoftTimer.h

---
+++ 003_softTimer/include/SoftTimer.h
@@ -0,0 +1,53 @@
+#include <Arduino.h>
+
+// ---------------------------------------------------------------
+class SoftTimer {
+private:
+  typedef void (*CallbackFunction)();
+  CallbackFunction callback;
+  uint32_t interval;
+  volatile uint32_t prevTime;
+  volatile uint32_t nowTime;
+  bool f_start;  //główna flaga włączająca timer
+
+public:
+  /*
+  timeInterval - intrerwał dla CallbackFunction
+  cb - nazwa CallbackFunction
+  OnOff - timer startuje od razu(true) lub po wywołaniu metody start
+  */
+  SoftTimer(uint32_t timeInterval, CallbackFunction cb, bool OnOff) {
+    f_start = false;
+    callback = cb;
+    interval = timeInterval;
+    if (OnOff) start();
+  }
+
+  // uruchomienie timera jeżeli nie został uruchomiony przy tworzeniu obiektu
+  void start() {
+    prevTime = millis();
+    f_start = true;
+  }
+
+  // zatrzymanie timera
+  void stop() {
+    f_start = false;
+  }
+
+  // restart timera
+  void restart(uint32_t newTimeInterval, CallbackFunction cb, bool OnOff) {
+    SoftTimer(newTimeInterval, cb, OnOff);
+  }
+
+  // metoda która musi być uruchomiona w głównej pętli programu żeby timer wogóle działał
+  void update() {
+    if (f_start) {
+      nowTime = millis();
+      if ((prevTime + interval) <= nowTime) {
+        callback();
+        prevTime = nowTime;
+      }
+    }
+  }
+};
+// ---------------------------------------------------------------
--- 003_softTimer/src/main.cpp
+++ 003_softTimer/src/main.cpp
@@ -1,53 +1,5 @@
 #include <Arduino.h>
-
-// ---------------------------------------------------------------
-class SoftTimer {
-private:
-  typedef void (*CallbackFunction)();
-  CallbackFunction callback;
-  uint32_t interval;
-  volatile uint32_t prevTime;
-  volatile uint32_t nowTime;
-  bool f_start;  //główna flaga włączająca timer
-
-public:
-  /*
-  timeInterval - intrerwał dla CallbackFunction
-  cb - nazwa CallbackFunction
-  OnOff - timer startuje od razu(true) lub po wywołaniu metody start
-  */
-  SoftTimer(uint32_t timeInterval, CallbackFunction cb, bool OnOff) {
-    f_start = false;
-    callback = cb;
-    interval = timeInterval;
-    if (OnOff) start();
-  }
-
-  void start() {
-    prevTime = millis();
-    f_start = true;
-  }
-
-  void stop() {
-    f_start = false;
-  }
-
-  void restart(uint32_t newTimeInterval, CallbackFunction cb, bool OnOff) {
-    SoftTimer(newTimeInterval, cb, OnOff);
-  }
-
-  void update() {
-    if (f_start) {
-      nowTime = millis();
-      if ((prevTime + interval) <= nowTime) {
-        callback();
-        prevTime = nowTime;
-      }
-    }
-  }
-};
-
-// ---------------------------------------------------------------
+#include "SoftTimer.h"
 
 uint32_t ticks = 0;
 void onTimerEnd() {
