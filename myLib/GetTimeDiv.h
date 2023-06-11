d85d3494ad252139399785db73e0db9f3d9fa744
DamianPodrazaCode <damian.podraza@gmail.com>
2023-06-11 17:14:19 +0200

    modified:   myLib/GetTimeDiv.h

--- myLib/GetTimeDiv.h
+++ myLib/GetTimeDiv.h
@@ -1,11 +1,41 @@
 #ifndef GetTimeDiv_h
 #define GetTimeDiv_h
 
-class GetTimeDiv {
-private:
-public:
-    GetTimeDiv() {}
-};
+//#include <inttypes.h>
+#include <Arduino.h>
 
+class GetTimeDiv {
+
+private:
+    uint32_t divTime;
+    uint32_t tempTime;
+    uint32_t divTimeMicros;
+    uint32_t tempTimeMicros;
+
+public:
+    void start() {
+        tempTime = millis();
+    }
+
+    void end() {
+        divTime = millis() - tempTime;
+    }
+
+    void startMicros() {
+        tempTimeMicros = micros();
+    }
+
+    void endMicros() {
+        divTimeMicros = micros() - tempTimeMicros;
+    }
+
+    uint32_t getLastDiv() {
+        return divTime;
+    }
+
+    uint32_t getLastDivMicros() {
+        return divTimeMicros;
+    }
+};
 
 #endif // GetTimeDiv_h
